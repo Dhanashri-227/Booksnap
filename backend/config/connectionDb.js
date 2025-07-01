9ca24b9ffd31c21f0f8b424e86e91678124c57b7
Dhanashri-227 <ui24cs20@iiitsurat.ac.in>
2025-07-01 18:20:02 +0530

    Update connectionDb.js

--- backend/config/connectionDb.js
+++ backend/config/connectionDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const uri = process.env.MONGO_URI;
+    const uri = 'mongodb+srv://dhanshri1701:QMBtQJNM0o2NEkoX@cluster0.upc1pwu.mongodb.net/';
     if (!uri) {
       throw new Error("MONGO_URI is not defined in .env file");
     }
